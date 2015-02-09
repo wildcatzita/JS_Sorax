88fd2dfab12ef50b1f98533e410799ef94c03639
unknown <wildcatzita@gmail.com>
2015-02-09 11:51:24 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,72 @@
+// // Простые типы
+// var myVariable = 20,
+// 	myString = "String some",
+// 	myBool = true;
+// 	myNull = null,
+// 	myUndef = undefined;
+
+
+// console.log(typeof myVariable);
+// console.log(typeof myString);
+// console.log(typeof myBool);
+// console.log(typeof myNull);
+// console.log(typeof myUndef);
+
+
+// // Объектные типы
+
+
+// var obj = {name: "Zita"},
+// 	array = [1,2,3],
+// 	regexp = /w+/g,
+// 	func = function (){};
+
+// console.log("");
+// console.log(typeof obj);
+// console.log(typeof array);
+// console.log(typeof regexp);
+// console.log(typeof func);
+
+// console.log(obj);
+
+// var n = 12.67;
+// console.log(typeof n);
+// console.log(n.toFixed(2));
+// console.log(n.toExponential(4));
+// console.log(n.toPrecision(3));
+
+// var string = "Sometimes the same is different";
+// console.log(string.charAt(5));
+// console.log(string.length);
+// console.log(string.charAt(string.length - 1));
+// console.log(string.substring(10));
+// console.log(string.substring(10,21));
+// console.log(string.slice(-9));
+// console.log(string.substr(10,5));
+// console.log(string.indexOf("i"));
+// console.log(string.split(" "));
+// console.log(string.toUpperCase());
+// console.log(string.toLowerCase());
+
+
+/*console.log(true);
+console.log(false);
+console.log(Boolean(5));
+
+// False values
+
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(0));
+console.log(Boolean(NaN));
+console.log(Boolean(""));*/
+
+console.log(5 + "5");
+console.log(typeof(5 + "5"));
+console.log("5" * "4");
+console.log(typeof("5" * "4"));
+console.log("5" * "hi");
+console.log(typeof("5" * "hi"));
+console.log(Number("555"));
+
+
